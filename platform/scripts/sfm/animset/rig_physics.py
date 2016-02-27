134ae4bd03e308a0888e49fb67377d663adb878f
ClintonM0 <ClintonM0@users.noreply.github.com>
2016-02-27 14:44:44 +1100

    derp

--- platform/scripts/sfm/animset/rig_physics.py
+++ platform/scripts/sfm/animset/rig_physics.py
@@ -40,11 +40,11 @@ shot = sfm.GetCurrentShot()
 animSet = sfm.GetCurrentAnimationSet()
 model = animSet.gameModel
 
-mdl_path = RelativePathToFullPath(model.GetModelName(), game())
-if mdl_path is None:
-	phys_path = "<FILE NOT FOUND>"
-else:
-	phys_path = os.path.splitext(mdl_path)[0] + ".physics.txt"
+#mdl_path = RelativePathToFullPath(model.GetModelName(), game())
+#if mdl_path is None:
+phys_path = "<FILE NOT FOUND>"
+#else:
+#	phys_path = os.path.splitext(mdl_path)[0] + ".physics.txt"
 
 data = dict()
 
